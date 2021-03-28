2aa1cc7a5640b7f55309bb5c1aba00d45d63d437
Francisco <jhersonhuacho@gmail.com>
2021-03-27 23:47:02 -0500

    05. useEffect - Precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 export const Message = () => {
 
+  const [coords, setCoords] = useState({ x:0, y:0 });
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log('useEffect','Componente Message montado');
+    console.log('useEffect','Componente Message montado');    
+
+    const mouseMove = (e) => {
+      console.log('addEventListener', 'mousemove', e.x, e.y);
+      const coords = {x: e.x, y: e.y};
+      setCoords(coords);
+    }
+
+    window.addEventListener('mousemove', mouseMove);
+
     return () => {
       //cleanup
       // Esto se puede utilizar para cancelar suscripciones entre otras cosas
       console.log('useEffect','Componente Message DESmontado');
+      window.removeEventListener('mousemove', mouseMove);
     }
   }, [])
 
   return (
     <div>
       <h3>Eres genial</h3>
+      <p>
+        x:{ x } y : { y }
+      </p>
     </div>
   )
 }
