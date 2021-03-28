fb1f5d799d24f27e909793c1c4f06fc92e8ed408
Francisco <jhersonhuacho@gmail.com>
2021-03-28 00:14:17 -0500

    06. Formulario con Custom Hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export const useForm = (initialState = {}) => {
+  const [values, setValues] = useState(initialState);
+  //Podemos poner cualquier cantidad de lógica aquí en el customHook
+  // Podemos pasarle otros argumentos que sean las reglas de validación, o que campos son obligatorios  
+
+  const handleInputChange = (e) => {
+    // console.log(e.target);
+    // console.log(e.target.type);
+    console.log('useForm', 'handleInputChange',e.target.name);
+    console.log('useForm', 'handleInputChange',e.target.value);
+    
+    setValues({
+      ...values,
+      [e.target.name]:e.target.value
+    });
+
+  }
+
+  return [values, handleInputChange];
+
+}
