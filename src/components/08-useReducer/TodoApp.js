91c9f9dad77eff2e0c19d5350b569a906c4a01c0
Francisco <jhersonhuacho@gmail.com>
2021-03-28 18:39:31 -0500

    14. useReducer - y creando el estilo del TODO

---
+++ src/components/08-useReducer/TodoApp.js
@@ -0,0 +1,57 @@
+import React, { useReducer } from 'react';
+import './TodoApp.css';
+import { todoReducer } from './todoReducer';
+
+const initialState = [{
+  id: new Date().getTime(),
+  desc: 'Aprender React',
+  done: false
+}]
+
+export const TodoApp = () => {
+
+  const [todos] = useReducer(todoReducer, initialState);
+
+  console.log(todos);
+
+  return (
+    <div>
+      <h1>TodoApp({ todos.length })</h1>
+      <hr/>
+
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+          {
+            todos.map((todo, indice) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">{indice + 1} {todo.desc}</p>
+                <button className="btn btn-danger">
+                  Borrar
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr/>
+          <form>
+            <input 
+              type="text"
+              name="description"
+              placeholder="Aprender..."
+              autoComplete="off"
+              className="form-control"
+            />
+            <button className="btn btn-outline-primary mt-1 btn-block">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  )
+}
